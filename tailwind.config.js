dabaa870e62d728a77186a37d12dbad5ee317cff
Falaloon <sr.ratchatapusit@gmail.com>
2023-10-11 15:05:19 +0700

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      IBM: ["IBM Plex Sans Thai Looped", "sans-serif"],
+      regularIBM: ["IBM Plex Sans Thai Looped", "sans-serif"],
+      semiboldIBM: ["IBM Plex Sans Thai", " sans-serif"],
+      IBMPlexSans: ["IBM Plex Sans", "sans-serif"],
     },
     extend: {},
+    colors: {
+      bodycolor: "#ccd8e0",
+      textcolor: "#4b3422",
+      bodygreen: "#a0b5a7",
+      stronggreen: "#4f776a",
+      white: "#ffffff",
+      blue: "#22668D",
+      y1:"#FFCC70"
+    },
   },
   plugins: [],
 };
